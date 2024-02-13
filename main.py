db3f966f1e87dfb894e3bf4c991dfaf985366f9b
ogutsevda <sevda.ogut@epfl.ch>
2024-02-13 17:40:04 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Find the connected components in random graphs with the following algorithm:
+# -- Start from random node and perform BFS
+# -- Label the nodes that BFS visits
+# -- If all nodes are visited, the network is connected
+# -- Otherwise find an unvisited node and repeat BFS
+# NOTE: ChatGPT is not allowed but GitHub Copilot is fine :)
